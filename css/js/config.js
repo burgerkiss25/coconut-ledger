6af8f4d8fc83f74234e24893778bed192324262f
burgerkiss25 <burgerkiss25@gmail.com>
2025-12-31 13:48:51 +0000

    Create config.js

---
+++ css/js/config.js
@@ -0,0 +1,8 @@
+export const SUPABASE_URL = "https://urxhkwozkuwvhlvswuvm.supabase.co";
+export const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+
+export const DEFAULTS = {
+  sellPrice: 7,
+  duePer: 6,
+  bonusPct: 10
+};
